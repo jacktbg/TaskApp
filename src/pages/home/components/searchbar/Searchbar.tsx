82b553c8a3967940e43f3224e06af80dd174f7d6
jacktbg <jack.bustinza.work@gmail.com>
2025-06-12 17:11:33 -0500

    feat: Add profile searchbar behavior to show my profile component

--- src/pages/home/components/searchbar/Searchbar.tsx
+++ src/pages/home/components/searchbar/Searchbar.tsx
@@ -3,16 +3,23 @@ import { CancelIcon } from "../../icons/Icons"
 import styles from "./styles/searchbar.module.scss"
 import { InputWrapper } from "./components/InputWrapper"
 import { NotificationWrapper } from "./components/NotificationWrapper"
+import { useSearchFormStore } from "../../../../store/useStore"
 
 export const Searchbar = () => {
   const [focus, setFocus] = useState<boolean>(false)
+  const reset = useSearchFormStore((state) => state.reset)
   return (
     <section className={styles.searchbarContainer}>
       <div className={styles.searchbarWrapper}>
         <InputWrapper setFocus={setFocus} focus={focus} />
 
         {focus ? (
-          <div className={styles.iconWrapper}>
+          <div
+            className={styles.iconWrapper}
+            onMouseDown={() => {
+              reset()
+            }}
+          >
             <CancelIcon className={styles.icon} />
           </div>
         ) : null}
