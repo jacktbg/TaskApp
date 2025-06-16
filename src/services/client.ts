6f4c08b1efee0350c7e728978577d481dfcb994f
jacktbg <jack.bustinza.work@gmail.com>
2025-06-16 14:32:21 -0500

    feat: add .env file to store the JWT and use it in client.ts

--- src/services/client.ts
+++ src/services/client.ts
@@ -11,8 +11,8 @@ const httpLink = new HttpLink({
 })
 
 const authLink = setContext((_, { headers }) => {
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwb3NpdGlvbklkIjoiNmU1OGNlMzMtMjdkMi00OWM2LTg4ZmMtNmMxYTEzZjZlNGUwIiwicHJvamVjdElkIjoiNGJjZDUyNmYtNzNkNS00NGFiLThlYmEtM2UzZDJkMjY2NGQxIiwiZnVsbE5hbWUiOiJKYWNrIEJ1c3RpbnphIiwiZW1haWwiOiJqYWNrYnVzdGluemFAcmF2bi5jbyIsImlhdCI6MTc0ODQ0NzMwOX0.zui9-EyO6RaXvG9dCzem0NnPG8itWc8dTZ0x8mg-IZ0"
+  const token = import.meta.env.VITE_GRAPHQL_API_TOKEN
+  console.log(token)
   return {
     headers: {
       ...headers,
